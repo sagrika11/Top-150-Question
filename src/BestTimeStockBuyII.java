75c05ff27e11a829e9fee56b191e0156bf74c28c
Sagrika <Sagrikasoni5421@gmail.com>
2026-05-05 01:19:34 +0530

    LeetCode 122

---
+++ src/BestTimeStockBuyII.java
@@ -0,0 +1,11 @@
+public class BestTimeStockBuyII {
+    public static void main (String[] args){
+        int []prices ={7,1,5,3,6,4};
+        int maxProfit =0;
+        for(int i =1;i<prices.length;i++){
+            if(prices[i]>prices[i-1])
+                maxProfit+= prices[i]-prices[i-1];
+        }
+        System.out.println(maxProfit);
+    }
+}
